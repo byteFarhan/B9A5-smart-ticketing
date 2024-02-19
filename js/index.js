0590a6d9a7e5b1b3bf5e85799f39ecc369771aa0
farhan-mazumder <farhanmazumder75@gmail.com>
2024-02-19 20:43:30 +0600

    added modal and modified some sections

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const totalSeats = 40;
-const totalBooking = parseInt(
-  document.getElementById("total-booking").innerText
-);
-console.log(totalBooking);
-const seatsLeft = (document.getElementById("seats-left").innerText =
-  totalSeats - totalBooking);
--- js/utlity.js
+++ js/utlity.js
@@ -1,3 +1,6 @@
 function changeElementBg(elementId) {
-  document.getElementById(elementId).style.backgroundColor = "#1DD100";
+  //   document.getElementById(elementId).style.backgroundColor = "#1DD100";
+  document.getElementById(elementId).classList.add("bg-primary", "text-white");
 }
+
+// changeElementBg("A1");
